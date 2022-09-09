e0c5cbb581b950d1ac551fce9aa598044162a884
Aleksey Sidorenko <verzevun@gmail.com>
2022-09-09 15:12:46 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ru.verzevun.compressionencodingalgorithms {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ru.verzevun.compressionencodingalgorithms to javafx.fxml;
+    exports ru.verzevun.compressionencodingalgorithms;
+}
